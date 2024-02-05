fe177550934e6adbd6f5bb7647e2efaac082bd38
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-02-05 19:46:17 +0000

    Added basic JS and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings:
+      "<strong>Step 1</strong> Preheat an air fryer to 325 degrees F (165 degrees C) according to manufacturer's instructions.",
+    autoStart: true,
+    delay: 1.5,
+  });
+}
+
+let recipeIngredients = document.querySelector("#recipe-ingredients");
+recipeIngredients.addEventListener("submit", generateRecipe);
