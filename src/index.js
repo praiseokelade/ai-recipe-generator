fec5a7fccf996fa308ed00ead009547eb16a129e
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-02-10 23:44:01 +0000

    Edited context of AI API

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ function generateRecipe(event) {
   let userIngredients = document.querySelector("#user-ingredients");
   let apiKey = `017e2af483beec2b2e23fafdtf1f298o`;
   let prompt = `Use the ${userIngredients.value} to generate a quick and easy recipe`;
-  let context = `You are a food lover that has travelled all over the world cooking and eating different delicacies. Please make sure to use the user's ingredients in the recipe. Always display the ingredients and instructions in basic HTML with a <br /> between each paragraph.`;
+  let context = `You are a food lover that has travelled all over the world cooking and eating different delicacies. Please make sure to use the user's ingredients in the recipe. ALWAYS display the ingredients and instructions in basic HTML with a <br /> between EACH line. Do NOT display the recipe as one full paragraph.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let loadingSign = document.querySelector("#recipe");
-  loadingSign.innerHTML = "Generating a delicious recipe😋...";
+  loadingSign.classList.remove("hidden");
+  loadingSign.innerHTML = `Generating a delicious recipe using ${userIngredients.value} 😋...`;
   axios.get(apiUrl).then(showRecipe);
 }
 
